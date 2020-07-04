9993b4db6355ab9d3f81636f45a3d4761602c74d
yuanchuang888 <307280734@qq.com>
2020-07-05 00:19:43 +0800

    创建分支

--- src/main/java/git/HelloGit.java
+++ src/main/java/git/HelloGit.java
@@ -6,6 +6,7 @@ public class HelloGit {
 		// TODO Auto-generated method stub
          System.out.println("hello git");
          System.out.println("更新1");
+         System.out.println("创建分支");
 	}
 
 }
